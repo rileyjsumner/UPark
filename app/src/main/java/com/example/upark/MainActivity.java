ed71badfdcda2d86bb5ce78590f759f7b16ce366
Garrett Rodgers <gcrodgers@wisc.edu>
2021-10-21 17:05:54 -0500

    Added some of the login screen forms and functionality - Rodgers

--- app/src/main/java/com/example/upark/MainActivity.java
+++ app/src/main/java/com/example/upark/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.upark;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void login(View view) {
+
+    }
+
+    public void create_account(View view) {
+        TextView createAccount = (TextView) findViewById(R.id.createAccount);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateAccount.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void forgot_password(View view) {
+        TextView forgotPassword = (TextView) findViewById(R.id.forgotPassword);
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ForgotPassword.class);
+                startActivity(intent);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
