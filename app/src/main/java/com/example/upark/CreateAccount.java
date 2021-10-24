d55fcdd9294de5ea5733fe985904d7ecc7e2e484
sakuniego <egodawatte@wisc.edu>
2021-10-24 13:42:22 -0500

    placed elements, still need to fix some constraints

--- app/src/main/java/com/example/upark/CreateAccount.java
+++ app/src/main/java/com/example/upark/CreateAccount.java
@@ -3,6 +3,8 @@ package com.example.upark;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sec_q_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
