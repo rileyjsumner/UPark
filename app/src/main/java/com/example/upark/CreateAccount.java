a09be52b757a75d83a5a2262af5d425310c8db8e
sakuniego <egodawatte@wisc.edu>
2021-10-24 19:40:26 -0500

    rearranged layout

--- app/src/main/java/com/example/upark/CreateAccount.java
+++ app/src/main/java/com/example/upark/CreateAccount.java
@@ -3,10 +3,20 @@ package com.example.upark;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class CreateAccount extends AppCompatActivity {
+    // when submit button pressed:
+    public void clickFunction(View view) {
+        // check if username already exists
+
+        // create new user and save data
+        //      where to save security question? do first name and last name fields need to be added?
+
+        // go to home page or profile page
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
